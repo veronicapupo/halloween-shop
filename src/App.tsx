2ac3e85b5a1f74983f66fd4e6cc8949c35811be2
veronica pupo <veronica.pupo@hotmail.com>
2022-10-04 09:08:45 -0300

    criacao do projeto

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return(
+    // <BrowserRouter>
+    // <Navbar />
+    // <div style={{ minHeight: '100vh' }}>
+    // <Routes> // Antigo Switch
+    // <Route path="/" element={<Login />} />
+    // <Route path="/login" element={<Login />} />
+    // <Route path="/home" element={<Home />} />
+    // <Route path="/cadastro" element={<CadastroUsuario />} />
+    // </Routes>
+    // </div>
+    // <Footer />
+    // </ BrowserRouter >
+    <>
+    </>
+    )
+}
+
+export default App;
